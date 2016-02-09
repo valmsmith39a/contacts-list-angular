4d82afa7e51ba3b4e378127f62de7d7e1c02ba7c
valmsmith39a <valmsmith39a@gmail.com>
2016-02-08 20:17:57 -0800

    Local Storage via ngStorage working

--- main.js
+++ main.js
@@ -4,23 +4,48 @@ console.log('in main.js');
 // use ngStorage
 var app = angular.module('MyApp', ['ngStorage']); // instantiating a NEW module named "MyApp"
 
+
+
 app.controller('mainCtrl', function($scope, $localStorage) {
   console.log('hello from main controller!');
   $scope.$storage = $localStorage;
   $scope.potions = [];
   $scope.keys = Object.keys;
 
+  $scope.readPotion = function(){
+    console.log('in read potion');
+    var itemsArray = $localStorage.itemsArray || [];
+    console.log('in read potion');
+    $scope.potions = itemsArray; 
+  };
+
+  $scope.init = function() {
+     $scope.readPotion();
+  };
+
+  $scope.init(); 
+
   $scope.addPotion = function(){
     console.log('in addItem()');
     console.log('new potion is: ', $scope.newPotion);
+    
     $scope.potions.push($scope.newPotion);
-    $localStorage.itemsArray = $scope.potions;    
+    
+    var itemsArray = $localStorage.itemsArray || [];
+    itemsArray.push($scope.newPotion);
+    $localStorage.itemsArray = itemsArray;     
     $scope.newPotion = {};
   };
 
   $scope.deletePotion = function(potion){
     console.log('in deleteItem()');
+    console.log(' items array is: ', $localStorage.itemsArray);
+
     var itemIndex = $scope.potions.indexOf(potion);
     $scope.potions.splice(itemIndex, 1);
+
+    var itemsArray = $localStorage.itemsArray || [];
+    itemsArray.splice(itemIndex, 1);
+    $localStorage.itemsArray = itemsArray;   
   };
 });
