79ceccff81d4846a090afdac376e388f9e28bc81
valmsmith39a <valmsmith39a@gmail.com>
2016-02-08 21:33:45 -0800

    Delete function works

--- main.js
+++ main.js
@@ -5,22 +5,10 @@ console.log('in main.js');
 var app = angular.module('MyApp', ['ngStorage']); // instantiating a NEW module named "MyApp"
 
 app.controller('mainCtrl', function($scope, $localStorage) {
-
-  console.log('hello from main controller!');
-  console.log('before localStorage');
-  /*
-  $scope.$storage = $localStorage.$default({
-    counter: 42
-   });
-  */
-  $scope.$storage = $localStorage;
-  //$localStorage = $localStorage.$default({itemsArray:[1,1,1]});     
-  
+  $scope.$storage = $localStorage;  
   $scope.keys = Object.keys;
 
-
   $scope.readPotion = function(){
-    console.log('in read potion');
     if(!$localStorage.itemsArray) {
       $localStorage.itemsArray = [];  
       $scope.potions = $localStorage.itemsArray;
@@ -28,16 +16,14 @@ app.controller('mainCtrl', function($scope, $localStorage) {
       $scope.potions = $localStorage.itemsArray;
     }
   };
-  
+
   $scope.init = function() {
-     console.log('inside init');
      $scope.readPotion();
   };
 
   $scope.init(); 
 
   $scope.addPotion = function(){
-    //$scope.potions.push($scope.newPotion);
     var itemsArray = $localStorage.itemsArray || [];
     itemsArray.push($scope.newPotion);
     $localStorage.itemsArray = itemsArray;     
@@ -45,14 +31,7 @@ app.controller('mainCtrl', function($scope, $localStorage) {
   };
 
   $scope.deletePotion = function(potion){
-    console.log('in deleteItem()');
-    console.log(' items array is: ', $localStorage.itemsArray);
-
     var itemIndex = $scope.potions.indexOf(potion);
-    $scope.potions.splice(itemIndex, 1);
-
-    var itemsArray = $localStorage.itemsArray || [];
-    itemsArray.splice(itemIndex, 1);
-    $localStorage.itemsArray = itemsArray;   
+    $localStorage.itemsArray.splice(itemIndex, 1);
   };
 });
